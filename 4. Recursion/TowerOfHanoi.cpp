a0ffbfbac6939c1e811db9ccef83b0c3b272680e
casafurix <agnibha10@gmail.com>
2022-03-27 18:30:20 +0530

    commit 27/3/22

---
+++ 4. Recursion/TowerOfHanoi.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+void TOH(int n, char A, char B, char C)
+{
+    if (n == 1)
+    {
+        cout << "Move 1 from " << A << " to " << C << endl;
+        return;
+    }
+    TOH(n - 1, A, C, B);
+    cout << "Move " << n << " from " << A << " to " << C << endl;
+    TOH(n - 1, B, A, C);
+}
+
+int main()
+{
+    char A, B, C;
+    TOH(2, 'A', 'B', 'C');
+
+    return 0;
+}
---
+++ rough.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main()
+{
+    int x;
+    while (1)
+    {
+        cin >> x;
+        if (x == 42)
+            break;
+        cout << x << endl;
+    }
+    return 0;
+}
