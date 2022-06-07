f4a07a01f9b3bfeaf3ec1f28396af3546c33fffa
casafurix <agnibha10@gmail.com>
2022-06-07 08:26:10 +0530

    trapping rainwater (efficient)

--- 5. Arrays/TrappingRainwater.cpp
+++ 5. Arrays/TrappingRainwater.cpp
@@ -21,12 +21,34 @@ int getWaterNaive(int arr[], int n)
     return res;
 }
 
+int getWaterEff(int arr[], int n)
+{
+    int res = 0;
+    int lMax[n], rMax[n];
+
+    lMax[0] = arr[0];
+    for (int i = 1; i < n; i++)
+        lMax[i] = max(arr[i], lMax[i - 1]);
+
+    rMax[n - 1] = arr[n - 1];
+    for (int i = n - 2; i >= 0; i--)
+        rMax[i] = max(arr[i], rMax[i + 1]);
+
+    for (int i = 1; i < n - 1; i++)
+    {
+        res += min(lMax[i], rMax[i]) - arr[i];
+    }
+    return res;
+}
+
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    int arr[] = {5, 0, 6, 2, 3};
+    int arr[] = {3, 0, 2, 0, 4};
     cout << getWaterNaive(arr, 5);
+    cout << endl;
+    cout << getWaterEff(arr, 5);
 
     return 0;
 }
