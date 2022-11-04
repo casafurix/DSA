f6b3478a6ef51a563233837b79fa848b88041d35
Agnibha Sarkar <agnibha10@gmail.com>
2022-11-05 01:24:15 +0530

    correction in longest consec. subsequence

--- 9. Hashing/LongestConsecSubsequence.cpp
+++ 9. Hashing/LongestConsecSubsequence.cpp
@@ -9,7 +9,7 @@ int longestConsecSubsequenceNaive(int arr[], int n)
 {
     sort(arr, arr + n);
     int res = 1, curr = 1;
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i < n; i++)
     {
         if (arr[i] == arr[i - 1] + 1)
             curr++;
